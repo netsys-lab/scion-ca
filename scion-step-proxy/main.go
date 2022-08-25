fedfba27e13bcad05de717c3ff63b39a53cb1010
Marten Gartner <martenwallewein@web.de>
2022-08-25 12:35:42 +0200

    Add flags for configuration

--- scion-step-proxy/main.go
+++ scion-step-proxy/main.go
@@ -1,13 +1,28 @@
 package main
 
 import (
+	"flag"
 	"net/http"
 
 	"github.com/netsys-lab/scion-step-proxy/api"
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	loglevel     = flag.String("loglevel", "INFO", "Log level (ERROR|WARN|INFO|DEBUG|TRACE)")
+	local        = flag.String("local", ":8088", "Local listen address (default: :3000)")
+	jwtSecrect   = flag.String("jwtSecrect", "", "Secret to generate JWT's (default: unset)")
+	certDuration = flag.String("certDuration", "1d", "Expiration Time of certs (default: 1d)")
+	trcPath      = flag.String("trcPath", "", "Path to trc files with the format $ISD-$base-$serial.trc (default: '')")
 )
 
 func main() {
+	flag.Parse()
 
-	r := api.NewApiRouter("", "lkasjdlkasjdlkasjdklsaj", "3d")
-	http.ListenAndServe(":3000", r.Router)
+	if jwtSecrect == nil || *jwtSecrect == "" {
+		logrus.Fatal("No jwtSecret provided")
+	}
+
+	r := api.NewApiRouter(*trcPath, *jwtSecrect, *certDuration)
+	http.ListenAndServe(*local, r.Router)
 }
