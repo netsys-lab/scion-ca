f5a387f5b55e54fe0f4a292ccd24ef1fe276357a
Marten Gartner <martenwallewein@web.de>
2023-02-22 22:05:51 +0100

    Add first working docker-compose deployment

---
+++ startup.sh
@@ -0,0 +1,15 @@
+# !/bin/sh
+
+if [ -d "/etc/step-ca/.step" ]; then
+    echo "/etc/step-ca/.step exists, CA was already initialized"
+else
+echo "Creating new step-ca"
+rm -rf /root/.step
+step ca init --key=$CA_ROOT_KEY --root=$CA_ROOT_CERT --provisioner=$SCION_CA_PROVISIONER --deployment-type=standalone --name=$SCION_CA_SUBJECT_COMMONNAME --dns=127.0.0.1 --address=127.0.0.1:8443 --password-file /etc/step-ca/scion-ca.pw
+cp /bin/step /etc/step-ca/step
+cp -R /root/.step/ /etc/step-ca/
+fi
+
+sleep infinity
+
+# step ca certificate --not-after=24h --provisioner-password-file=/etc/step-ca/scion-ca.pw --ca-url=https://127.0.0.1:8443 --root=/etc/step-ca/.step/certs/root_ca.crt "1-999 AS Certificate" /etc/step-ca/tmp.crt /etc/step-ca/tmp.key
